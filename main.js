5d2a5440565649a826b1446d1b2bfae6f2c0d426
Agnieszka Kurek <agniechakurek@gmail.com>
2016-05-19 14:12:30 +0200

    Generating objects with Object Create

--- main.js
+++ main.js
@@ -1,23 +1,37 @@
-var obj = {
-    firstName: "Jeremy",
-    lastName: "Appril",
+//var foo = {};
+//var bar = Object.create(Object.prototype);
+
+
+
+var johnDoe = {
+    firstName: "John",
+    lastName: "Doe",
+    sayName: function () {
+        return "My name is" + " " + this.firstName + " " + this.lastName;
+    }
 };
-//obj.firstName = "Jeremy";
-//obj["lastName"] = "Appril";
 
-var firstName = obj["firstName"];
-var lastName = obj.lastName;
-
-Object.defineProperty(obj, "country", {
-    value : "USA"
-});
-
-Object.defineProperties(obj, {
-    twitter : {
-        value :  "jwmcpeaak"
-    },
-    email: {
-        value : "email@gmail.com"
+var janeDoe = Object.create(johnDoe, {
+    firstName: {value: "Jane"},
+    greet:{
+        value: function (person) {
+        return "Hello," + " " + person.firstName;
+    }
     }
 });
+
+var jimSmith = Object.create(janeDoe, {
+    firstName: {value: "Jim"},
+    lastName: {value: "Smith"},
+   
+});
+
+
+//alert(johnDoe.sayName());
+//alert(janeDoe.sayName() + " " + janeDoe.greet(johnDoe));
+//alert(jimSmith.sayName() + " " + jimSmith.greet(janeDoe));
+
+console.log(janeDoe._proto_ === johnDoe);
+console.log(jimSmith._proto_ === janeDoe);
+
 
