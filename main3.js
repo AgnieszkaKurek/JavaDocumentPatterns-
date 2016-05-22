7deaaa8708adf6e5141d7095e79a2302f0561218
Agnieszka Kurek <agniechakurek@gmail.com>
2016-05-22 17:11:21 +0200

    using basic module pattern

--- main3.js
+++ main3.js
@@ -1,57 +1,42 @@
-define(function() {
+var dom = (function(){
     
-    var subscribers = {};
+   var _counter = 0,
+   
+   function generateId() {
+       return "customId" + _counter++;
+   }
+   
+   function create(tagName, id) {
+       var el = document.createElement(tagName);
+        el.id = id || generateId();
+        return el;
+   }
+   return {
+ generateId : generateId,
+    create: create,
+   };
     
-    
-    function subscribe(type, fn) {
-        if (!subscribers[type]) {
-            subscribers[type] = [];
-        }
-        if (subscribers[type].indexOf(fn) == -1) {
-            subscribers[type].push(fn);
-        }
-    }
-    function unsubscribe(type, fn) {
-        var listeners = subscribers[type];
-        if (!listeners) {
-            return;
-        }
-        var index = listeners.indexOf(fn);
-        if (index > -1) {
-            listeners.splice(index,1);
-        }
-    }
-    function publish(type, evtObj) {
-        if (!subscribers[type]) {
-            return;
-    }
-    
-    if (!evtObj.type){
-        evtObj.type = type;
-    }
-    var listeners = subscribers[type];
-    for (var ii = 0, ll = listeners.length; ii < ll; ii++) {
-        listeners[ii](evtObj);
-    }
-    
-    
-    return {
-        subscribe: subscribe,
-        unsubscribe: unsubscribe,
-        publish: publish,
-    };
-    };
+}());
 
 
 
 
 
-require(["eventmanager"], function(evtman) {
-    function foo(evt) {
-        consol.log(evt.mesage);
-    }
-    evtman.subscribe("test/foo", foo);
-    evtman.subscribe("test/foo", {message: "hello this is a custom event"});
-    evtman.subscribe("test/foo", foo);
-    evtman.subscribe("test/foo", {message: "this should not be"});
-});
+
+//var dom = {
+  //  _counter :0,
+  //  generateId : function() {
+       // return "customId" + this._counter++;
+        
+    //},
+    //create: function(tagName, id) {
+       // var el = document.createElement(tagName);
+        
+        //el.id = id || this.generateId();
+      //  if (id) {
+          //  el.id = id;
+       // }
+        
+      //  return el;
+    //}
+//};
